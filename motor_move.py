9e862d97bae904ce718283ea78c1f32d439bfd0a
DesignBuildInspire <designbuildinspireclub@gmail.com>
2023-11-13 21:25:49 -0800

    added melody example to play sound
    Added standard Firmata modified for mBot as well as an example for playing a melody on the bot

--- motor_move.py
+++ motor_move.py
@@ -4,27 +4,37 @@ from pyfirmata.boards import BOARDS
 
 import time
 
-# instantiate and register the cmd handler
+def main():
+    board = Arduino("/dev/tty.usbserial-1430",baudrate=115200)
 
-board = Arduino("/dev/tty.usbserial-1410",baudrate=115200)
-
-iterator = util.Iterator(board)
-iterator.start()
-
-# Dir1 pin on mCore is D7
-board.digital[7].write(0)
-# Dir1 pin on mCore is D4
-board.digital[4].write(1)
-
-motor1_pwm = board.digital[5]
-motor1_pwm.mode = PWM
-motor2_pwm = board.digital[6]
-motor2_pwm.mode = PWM
-
-motor1_pwm.write(0.5)
-motor2_pwm.write(0.5)
-time.sleep(2)
-motor1_pwm.write(0)
-motor2_pwm.write(0)
+    iterator = util.Iterator(board)
+    iterator.start()
+    
+    try:
+        number=int(input("enter number"))
+        print(number)
+    except ValueError:
+        print("There is a value error")    
+    
+    MoveMotor(board, 1,0.70,number)
 
 
+def MoveMotor(board, direction,speed=0.5,duration=1):
+    # Dir1 pin on mCore is D7
+    board.digital[7].write(0)
+    # Dir1 pin on mCore is D4
+    board.digital[4].write(1)
+
+    motor1_pwm = board.digital[5]
+    motor1_pwm.mode = PWM
+    motor2_pwm = board.digital[6]
+    motor2_pwm.mode = PWM
+
+    motor1_pwm.write(speed)
+    motor2_pwm.write(speed)
+    time.sleep(duration)
+    motor1_pwm.write(0)
+    motor2_pwm.write(0)
+    
+if __name__ == "__main__":
+    main()
