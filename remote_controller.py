40887a6c2dc9c4669a9a660855559acafa6ecebb
Amine J <aminej@Amines-MacBook-Pro-3.local>
2024-02-11 11:28:07 -0800

    Ela Baseline code

--- remote_controller.py
+++ remote_controller.py
@@ -12,7 +12,7 @@ initMotor(mbot)
 def on_key_release(key):
     try:
         if key.char == 'd':
-            move_arm(mbot,11,80)
+            move_arm(mbot , 11 , 80)
         if key.char == 'u':
             move_arm(mbot,11,100)
     except AttributeError:
@@ -34,31 +34,31 @@ def on_key_release(key):
 
 def on_key_press(key):
     try:
+        if key.char == 'w':
+            move_arm(mbot,12,0)
         if key.char == 'd':
-            move_arm(mbot,11,80)
-        if key.char == 'u':
-            move_arm(mbot,11,100)
+            move_arm(mbot,12,120)
         if key.char == 'm':
             play_sound(mbot,500,NOTE_A5)
     except AttributeError:
         if key == Key.right:
             print("Right key press")
-            startMove(mbot,RIGHT, 0.5)
+            startMove(mbot,RIGHT, 0.75)
         elif key == Key.left:
             print("Left key press")
-            startMove(mbot,LEFT, 0.5)
+            startMove(mbot,LEFT, 0.75)
         elif key == Key.up:
             print("Up key press")
-            startMove(mbot,FORWARD, 0.5)
+            startMove(mbot,FORWARD, 0.75)
         elif key == Key.down:
             print("Down key press")
-            startMove(mbot,BACKWARD, 0.5)
+            startMove(mbot,BACKWARD, 0.75)
 
         elif key == Key.esc:
             exit()     
 
 
-with keyboard.Listener(on_press=on_key_press,on_release=on_key_release) as listener:
+with keyboard.Listener(on_press = on_key_press , on_release = on_key_release) as listener:
     listener.join()
     
 # with keyboard.Listener(on_release=on_key_release) as listener:
